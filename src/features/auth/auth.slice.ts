24cb001d95386f57ed6b2edfea7b022483425154
krlkonstantine <kostea.023@gmail.com>
2023-05-11 08:52:43 +0300

    login simplified

--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -9,8 +9,7 @@ const register = createAsyncThunk('auth/register', (arg: any) => {
 
     })
 })
-const login = createAsyncThunk('auth/login', (arg: ArgLoginType, thunkApi) => {
-    const {dispatch} = thunkApi
+const _login = createAsyncThunk('auth/login', (arg: ArgLoginType, thunkApi) => {
     //создали санку, теперь подключаем ранее соданную апишку
     return authApi.login(arg).then((res) => {
         debugger
@@ -18,17 +17,17 @@ const login = createAsyncThunk('auth/login', (arg: ArgLoginType, thunkApi) => {
         return {profile: res.data}
     })
 })
+const login = createAsyncThunk('auth/login', async (arg: ArgLoginType, thunkApi) => {
+    const res = await authApi.login(arg)
+    return {profile: res.data}
+})
 
 const slice = createSlice({
     name: "auth",
     initialState: {
         profile: null as ProfileType | null
     },
-    reducers: {
-        // setProfile: (state, action: PayloadAction<{ profile: ProfileType }>) => {
-        //     state.profile = action.payload.profile
-        // }
-    },
+    reducers: {},
     extraReducers: builder => {
         builder.addCase(login.fulfilled, (state, action) => {
             state.profile = action.payload.profile
@@ -37,5 +36,5 @@ const slice = createSlice({
 })
 
 export const authReducer = slice.reducer
-// export const authActions = slice.actions
+//export const authActions = slice.actions
 export const authThunks = {register, login}
